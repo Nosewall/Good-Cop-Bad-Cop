a99df36a55790583ccb4cacf59d351861a18d986
Nolan N <nolannordwall@gmail.com>
2021-03-31 21:28:43 -0700

    Revert "small push to test"
    This reverts commit 1c517ff6f69477884c95611d9e0e01bc8ddee53e, reversing
    changes made to 0b34932b3112b12e73827dd0555bce5c22fca8c2.

--- scripts/journal scripts/journal_main.js
+++ scripts/journal scripts/journal_main.js
@@ -1,38 +1,27 @@
-
-// populates page with journal entries
 function getJournals(){
     firebase.auth().onAuthStateChanged(function (user) {
         db.collection("users").doc(user.uid)
-        .collection("journal entries") // grabs journal entries
-        .limit(7) // max 7 per page
-        .orderBy("date", "desc") // orders by date
+        .collection("journal entries")
+        .limit(7)
+        .orderBy("date", "desc")
         .get()
         .then(function(snap){
             let count = 0;
             snap.forEach(function(doc){
-                // grabs journal title
-                var t = doc.data().title;
-                // grabs text
-                var j = doc.data().journalText;
-                
-                if (t == ""){
-                    t = new Date(doc.data().date);                    
-                }
-                // used to create id's for generated cards
+                var d = doc.data().date;
+                var j = doc.data().jText;
                 ++count;
-                // Maximum number of characters displayed in preview
-                var maxCharDisplayed = 100;
-                // If over max trims preview
-                if (j.length >= maxCharDisplayed){
-                    j = j.substring(0,maxCharDisplayed) + "...";
+
+                if (j.length >= 100){
+                    j = j.substring(0,100) + "...";
                 }
-                // creates journal card
+
                 var journalCard = "<a id='journal-" + count + "' "
                 + "class='list-group-item list-group-item-action'>"
                 + "<div class='d-flex w-100 justify-content-between'><h5 class='mb-1'>"
-                + t + "</h5></div><p class='mb-1'>"
+                + d + "</h5></div><p class='mb-1'>"
                 + j + "</p></a>";
-                // places each new card before archive card
+
                 $("#go-to-archive").before(journalCard);
 
             })
--- scripts/notificationHandler.js
+++ scripts/notificationHandler.js
@@ -1,24 +1,3 @@
-// Variable used to store active coach.
-let activeCoach;
-// Pulls the active coach for user from db and stores in activeCoach.
-function getCoach(){
-    firebase.auth().onAuthStateChanged(function (user){
-        if(user){
-            db.collection("users")
-            .doc(user.uid)
-            .get()
-            .then(function(doc){
-                console.log(doc.data().selectedCoach);
-                activeCoach = doc.data().selectedCoach;
-            })
-        } else {
-            // no user signed in
-        }
-    })
-
-}
-
-// Get a phrase from the database using the coaches name and the key of the motivation you want to use
 function getPhrase(trainerName, motivation) {
     var phrase;
 
@@ -26,20 +5,15 @@ function getPhrase(trainerName, motivation) {
         .doc(trainerName)
         .get()
         .then(snapshot => {
-            phrase = snapshot.data()[motivation];
+            console.log(snapshot.data()[motivation]);
 
         })
 
-    return phrase
+
 }
 
-// Send an alert as a notification
-function sendNotification(trainerName, motivation){
-    phrase = getPhrase(trainerName, motivation);
-    alert(phrase);
-}
 
 function testSnapShot() {
     getPhrase("Decard", "checkIn1");
 }
-getCoach();
+testSnapShot();
