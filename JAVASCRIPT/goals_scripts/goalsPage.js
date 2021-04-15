0149a49937087e66b83edcacff402b7c9f137fd9
Nolan N <nolannordwall@gmail.com>
2021-04-14 22:40:28 -0700

    Goals page writting to database

--- JAVASCRIPT/goals_scripts/goalsPage.js
+++ JAVASCRIPT/goals_scripts/goalsPage.js
@@ -33,20 +33,56 @@ function newGoal() {
 }
 
 function submitGoal() {
-  var goalName = document.getElementById("goalName").nodeValue;
-  var all = document.getElementsByTagName("*");
-  for (var i = 0, max = all.length; i < max; i++) { //Get all elements on the page and loop through them
-    if (all[i].className == "stepBox") {
-      firebase.auth().onAuthStateChanged(function (user) {
-        db.collection("users").doc(user.uid)
-          .collection("goals").doc(goalName)
-          .set({
-            step: all[i],
-          });
-      })
+
+  //Add goal name to firebase
+  firebase.auth().onAuthStateChanged(function (user) {
+    //Get the goal name
+    var goalName = document.getElementById("goalName").value;
+    // Writes data to DB
+    if (user) {
+      var newGoal = db.collection("users")
+        .doc(user.uid).collection("goals");
+
+      newGoal.add({
+        Name: goalName,
+
+      }).then(function () {
+        console.log("Goal added to firebase by name");
+      });
+
+    } else {
+      // no user
     }
-  }
+  })
+
+  //Add steps to firebase
+  firebase.auth().onAuthStateChanged(function (user) {
+
+    var stepsDiv = document.getElementById("stepsDiv");
+    var stepCount = document.getElementById("stepsDiv").childElementCount;
+    var allSteps = stepsDiv.childNodes;
+    var index = 0;
+    console.log("Stepcount - " + stepCount);
+    while (index < stepCount.length) {
+      if (user) {
+        var newGoal = db.collection("users")
+          .doc(user.uid).collection("goals");
+
+        newGoal.add({
+          step: allSteps[index].Value,
+
+        }).then(function () {
+          console.log("Steps added to firebase");
+          window.location.replace("goals.html");
+        });
+
+      } else {
+        // no user
+      }
+    }
+  })
 }
+
 
 function cancelGoal() {
   window.location.replace("goals.html");
